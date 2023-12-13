4faf152fb8d34a040cb9c78b4961aea604c42296
Juuddi <justinrudi26@gmail.com>
2023-12-13 17:05:10 -0600

    Deploy + link QRC20

---
+++ polling-logs/index.js
@@ -0,0 +1,2 @@
+const { quais } = require('quais')
+
--- query-block/index.js
+++ query-block/index.js
@@ -1,1 +1,20 @@
 const { quais } = require('quais')
+
+// Create provider to query against
+const provider = new quais.providers.JsonRpcProvider(`https://rpc.cyprus1.colosseum.quaiscan.io`)
+
+// Block number to query
+const blockNumber = '0x1234'
+
+const main = async () => {
+	// get simplified block data (pass the second argument as true)
+	const simplifiedBlock = await provider.getBlock(blockNumber, true)
+
+	// get non-simplified block data (pass the second argument as false or omit it)
+	const block = await provider.getBlock(blockNumber)
+
+	console.log('\nThe simplified data for block number ' + blockNumber + ' is: \n\n' + JSON.stringify(simplifiedBlock))
+	console.log('\nThe non-simplified data for block number ' + blockNumber + ' is: \n\n' + JSON.stringify(block) + '\n')
+}
+
+main()
